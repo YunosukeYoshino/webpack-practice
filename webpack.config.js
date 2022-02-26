42eea69f8710d52355d5254c661b39bc47922af6
yunosuke <26004836+YunosukeYoshino@users.noreply.github.com>
2022-02-27 03:09:05 +0900

    pugでHTMLを作成

--- webpack.config.js
+++ webpack.config.js
@@ -25,9 +25,9 @@ module.exports = {
             },
             {
                 test: /\.png|\.jpg/,
-                type:'asset/resource',
-                generator:{
-                    filename:'images/[name][ext]'
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name][ext]'
                 },
                 use: [
                     // {
@@ -39,17 +39,36 @@ module.exports = {
                     // },
                 ],
             },
+            {
+                test: /\.pug/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                    },
+                    {
+                        loader: 'pug-html-loader',
+                        options: {
+                            pretty: true,
+                        },
+                    },
+                ],
+            },
         ],
     },
     stats: {
         children: true,
-      },
+    },
     plugins: [
         new MiniCssExtractPlugin({
             filename: './stylesheets/main.css',
         }),
         new HtmlWebpackPlugin({
-            template: './src/templates/index.html',
+            template: './src/templates/index.pug',
+            filename: 'index.html',
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/templates/access.pug',
+            filename: 'access.html',
         }),
         new CleanWebpackPlugin(),
     ],
